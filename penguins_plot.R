f5f12d25469367cad93389e78db9e11bc0b26f86
Adrian-AlvarezMolina <62895498+Adrian-AlvarezMolina@users.noreply.github.com>
2020-08-28 09:58:08 +0200

    Update penguins_plot.R
    Insert png of penguins species to a barplot

--- penguins_plot.R
+++ penguins_plot.R
@@ -1,4 +1,7 @@
-library(tidyverse); library(palmerpenguins); library(cowplot); library(magick)
+library(tidyverse)
+library(palmerpenguins)
+library(cowplot)
+library(magick)
 # Prepare the data to plot ------------------------------------------------
 
 my_data <- penguins
@@ -20,36 +23,50 @@ my_data_without_NA %>% count(species) %>%
 theme_set(theme_cowplot())
 
 bar_plot <- my_data_without_NA %>%
-              ggplot(aes(x = species)) +
-              geom_bar() +
-              ylab("Number of penguins") +
-              ggtitle("Frequence and type of Penguins in palmerpenguins dataset", 
-                      subtitle = "Knowing who are behind the numbers") +
-              scale_y_continuous(n.breaks = 5, 
-                                 breaks = seq(0,200,50), 
-                                 labels = c(0,50,100,150,200),
-                                 limits = c(0,400) ) +
-              theme(plot.title = element_text(hjust = 0.5),
-                    plot.subtitle = element_text(hjust = 0.5),
-                    axis.text = element_text(size = 20)) 
+  ggplot(aes(x = species)) +
+  geom_bar() +
+  ylab("Number of penguins") +
+  ggtitle("Frequence and type of Penguins in palmerpenguins dataset", 
+          subtitle = "Knowing who are behind the numbers") +
+  scale_y_continuous(n.breaks = 5, 
+                     breaks = seq(0,200,50), 
+                     labels = c(0,50,100,150,200),
+                     limits = c(0,400) ) +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5),
+        axis.text = element_text(size = 20)) 
 bar_plot
-path_to_png <- 
-  "/Users/Adrian/Desktop/Analiza tus datos/Imágenes y scripts de posts"
+
+path_to_adelie_image <- 
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Adelie_Penguin.jpg/429px-Adelie_Penguin.jpg"
+path_to_gentoo_image <- 
+  "https://upload.wikimedia.org/wikipedia/commons/9/95/Gentoo_Penguin_at_Cooper_Bay%2C_South_Georgia.jpg"
+path_to_chinstrap_image <- 
+  "https://cdn.britannica.com/08/152708-050-23B255B3/Chinstrap-penguin.jpg"
 
 example_plot <- 
   ggdraw() +
-  draw_image(image = (paste(path_to_png, "/", "adelie_image.jpg", sep = "")),                 x = -0.26,
+  draw_image(image = path_to_adelie_image,  
+             x = -0.26,
              y = 0.15,
              scale = 0.5) +
-  draw_image(image = (paste(path_to_png, "/", "Chinstrap-penguin.jpg", sep = "")),                 x = 0.025,
+  draw_image(image = path_to_chinstrap_image,
+             x = 0.025,
              y = 0.15,
              scale = 0.5) +
-  draw_image(image = (paste(path_to_png, "/", "gentoo_penguin.jpg", sep = "")),                 x = 0.310,
+  draw_image(image = path_to_gentoo_image,
+             x = 0.310,
              y = 0.15,
              scale = 0.5) +
   draw_plot(bar_plot)
+# Can take some second to visualize the plot in R
+example_plot
 
-png(filename = paste(path_to_png, "/", "penguins_plot.png", sep = ""), 
+# Save the plot -----------------------------------------------------------
+# Modify the path to save the image where you want
+path_to_png <- "/Users/Adrian/Desktop"
+
+png(filename = paste(path_to_png, "/", "penguins_plot2.png", sep = ""), 
     height = 1000,
     width = 1500)
 example_plot
